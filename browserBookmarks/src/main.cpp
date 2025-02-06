99d73543f4080aa90dfb5ae52fc005e94a0bf995
Kyle <kylesch115@gmail.com>
2025-02-06 15:33:37 -0700

    setup json parsing (nlohmann)

--- browserBookmarks/src/main.cpp
+++ browserBookmarks/src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <string>
+#include <fstream>
+#include <stdexcept>
+
+using json = nlohmann::json;
 
 int main()
 {
-  std::cout << "bookmarks" << std::endl;   
+  const auto home = std::getenv("HOME");
+  std::string path = std::string(home) + "/.config/BraveSoftware/Brave-Browser/Default/Bookmarks";
+
+  std::ifstream f(path);
+  if (!f.is_open())
+  {
+    throw std::runtime_error("error: failed to open file " + path);
+  }
+
+  json data = json::parse(f);
+
+  std::cout << data.dump(4) << std::endl;
 }
