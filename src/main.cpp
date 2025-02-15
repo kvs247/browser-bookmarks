2d2950f75ece9c0476fa6356677c0f00aaf0ff6e
Kyle <kylesch115@gmail.com>
2025-02-14 17:49:33 -0700

    small UI update

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,14 @@ int main(int argc, char *argv[])
 {
   Manager manager;
   BrowserType browserType;
+  bool validBrowserSelected = false;
 
   const std::string arg = (argc > 1) ? argv[1] : "--help";
   if (arg == "--help")
   {
     std::cout << "~~~ Browser Bookmark Utility ~~~\n";
-    std::cout << "Supported Browsers:\n" << "  Brave: --brave\n" << "  Opera: --opera\n";
+    std::cout << "Download latest bookmarks file: --download\n";
+    std::cout << "Supported Browsers:\n" << "  Brave: --brave\n" << "  Opera: --opera\n" << "  Firefox: --firefox\n";
 
     return 0;
   }
@@ -24,17 +26,26 @@ int main(int argc, char *argv[])
   else if (arg == "--brave")
   {
     browserType = BrowserType::Brave;
+    validBrowserSelected = true;
   }
   else if (arg == "--opera")
   {
     browserType = BrowserType::Opera;
+    validBrowserSelected = true;
   }
   else if (arg == "--firefox")
   {
     browserType = BrowserType::Firefox;
+    validBrowserSelected = true;
   }
 
-  manager.uploadFromBrowser(browserType);
+  if (validBrowserSelected)
+  {
+    manager.uploadFromBrowser(browserType);
+    return 0;
+  }
+
+  std::cout << "unkown command: " << arg << "\n";
 
   return 0;
 }
